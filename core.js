a59a5279a4bdaae3829c0af25cd3d8fb30ad87ee
Mansurion <mansoor.kumratov@mail.ru>
2025-07-22 18:22:26 +0300

    add js big rework

---
+++ core.js
@@ -0,0 +1,10 @@
+const radioButtons = document.querySelectorAll('input[type="radio"]');
+const checkbox = document.getElementById('checkbox1');
+
+radioButtons.forEach(radio => {
+  radio.addEventListener('change', () => {
+    if (radio.checked) {
+      checkbox.checked = true;
+    }
+  });
+});
